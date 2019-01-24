dd763d5bb97117a037013e717552b44eb7f12a43
Péter Kovács <kovpeti@yahoo.com>
2019-01-24 11:56:46 +0000

    0.1.2.4
    Added FTimerInterval,Display signs set at once
    Did not set FActive property when Active went to false;

--- TPTIndustrial/source/tti1604comm.pas
+++ TPTIndustrial/source/tti1604comm.pas
@@ -26,6 +26,11 @@
 28/03/2018    <@> 0.1.0.0  First public release
 12/09/2018    <@> 0.1.1.0  Added getMeasurement
 14/09/2018    <@> 0.1.2.0  There was a bug, Port config must be after connect
+21/09/2018    <@> 0.1.2.1  Added command numbers
+25/09/2018    <@> 0.1.2.2  Modified getMeasurement to decrease delay
+25/09/2018    <@> 0.1.2.3  Added FDisableTimer
+12/10/2018    <@> 0.1.2.4  Added FTimerInterval,Display signs set at once
+                           Did not set FActive property when Active went to false;
 
   $Author$
   Peter Kovacs - PetiTech.tk
@@ -42,21 +47,21 @@ uses
 
 {Commands}
 const
-  cmdUp='a';
-  cmdDown='b';
-  cmdAuto='c';
-  cmdA='d';
-  cmdmA='e';
-  cmdV='f';
+  cmdUp='a';cmdUpNo=10;
+  cmdDown='b';cmdDownNo=11;
+  cmdAuto='c';cmdAutoNo=12;
+  cmdA='d';cmdANo=5;
+  cmdmA='e';cmdmANo=2;
+  cmdV='f';cmdVNo=4;
   cmdOperate='g';
-  cmdR='i';
-  cmdHz='j';
-  cmdShift='k';
-  cmdAC='l';
-  cmdDC='m';
-  cmdmV='n';
-  cmdRemote='u';
-  cmdLocal='v';
+  cmdR='i';cmdRNo=7;
+  cmdHz='j';cmdHzNo=8;
+  cmdShift='k';cmdShiftNo=9;
+  cmdAC='l';cmdACNo=6;
+  cmdDC='m';cmdDCNo=3;
+  cmdmV='n';cmdmVNo=1;
+  cmdRemote='u';cmdRemoteNo=14;
+  cmdLocal='v';cmdLocalNo=13;
 
 type
  ECustomTTi1604CommException = class(Exception);
@@ -70,29 +75,44 @@ type
     { Protected declarations }
     FPort:string;                   {Communication port}
     FActive:boolean;                {Open/Close communication}
+    FDisableTimer:boolean;          {Enable/Disable internal timer.
+                                     This feature allow to stop continousos reading if no display assigned.}
     FMessage:string;                {Received message from instrument}
     FRxData:array[0..9] of byte;    {Received data}
     FSerial:TBlockSerial;
     FDisplay:TTTi1604DsplPanel;
+    FUpdateInterval:Cardinal;
 
     procedure SetPort(NewPort:string);
     procedure SetActive(NewActive:boolean);
-    procedure SendDataToDisplay;              {Translates received data and set-up display}
+    {Translates received data and set-up display}
+    procedure SendDataToDisplay;
+    procedure SetDisableTimer(NewTimerStatus:boolean);
+    procedure SetUpdateInterval(Value: Cardinal);
 
   public
     { Public declarations }
     constructor Create(AOwner: TComponent); override;
     destructor Destroy; override;
-    procedure SendCommand(Command:integer);   {Send command to instrument}
-    procedure ReadComm(ASender:TObject);      {Timer OnTimer event, reading data from Comm port}
-    function getMeasurement:string;           {Convert data stream to string. Same then SendDataToDisplay but without display}
+    {Send command to instrument}
+    procedure SendCommand(Command:integer);
+    {Timer OnTimer event, reading data from Comm port}
+    procedure ReadComm(ASender:TObject);
+    {Convert the next available data stream to string. Same then SendDataToDisplay but without display}
+    function GetMeasurement:string;
   published
     { Published declarations }
 
-    property Port:string Read FPort Write SetPort;        {Communication port. On Windows Comx, on Linux /dev/TTYSx}
-    property Active:boolean Read FActive Write SetActive default false;                 {Open/Close communication}
-    property ReceivedMessage:string Read FMessage;       {Received message from instrument}
+    {Communication port. On Windows Comx, on Linux /dev/TTYSx}
+    property Port:string Read FPort Write SetPort;
+    {Open/Close communication}
+    property Active:boolean Read FActive Write SetActive default false;
+    {Enable/Disable internal timer}
+    property DisableTimer:boolean Read FDisableTimer Write SetDisableTimer default false;
+    {Received message from instrument}
+    property ReceivedMessage:string Read FMessage;
     property Display:TTTi1604DsplPanel Read FDisplay Write FDisplay;
+    property UpdateInterval: Cardinal Read FUpdateInterval Write SetUpdateInterval default 100;
   end;
 
   TTTi1604Comm = class(TCustomTTi1604Comm)
@@ -100,6 +120,8 @@ type
     property Display;
     property Port;
     property Active;
+    property DisableTimer;
+    property UpdateInterval;
   end;
 
 
@@ -112,62 +134,92 @@ var NumDisplay:string;
     i,j:integer;
     Units:byte;
     Range:byte;
+    DisplaySigns:TSigns;
 begin
-     if FRxData[0] <>13 then exit;
-     if not Assigned(FDisplay) then exit;
+     //if FRxData[0] <>13 then exit;
+     //if not Assigned(FDisplay) then exit;
      Units:=(FRxData[1] and %00000111);
      //Make invisible signs
-     FDisplay.Signs:=FDisplay.Signs-[skmVSign,skVSign,skHoldSign,skTHldSign,skmASign,skASign,
+     {FDisplay.Signs:=FDisplay.Signs-[skmVSign,skVSign,skHoldSign,skTHldSign,skmASign,skASign,
+                          skMinSign,skNullSign,skRSign,skkRSign,skMaxSign,skDSign,skHzSign,
+                          skAutoSign,skContSign];}
+     DisplaySigns:=DisplaySigns-[skmVSign,skVSign,skHoldSign,skTHldSign,skmASign,skASign,
                           skMinSign,skNullSign,skRSign,skkRSign,skMaxSign,skDSign,skHzSign,
                           skAutoSign,skContSign];
      //Make visible current unit
      case Units of
-          1:FDisplay.Signs:=FDisplay.Signs+[skmVSign];
+{          1:FDisplay.Signs:=FDisplay.Signs+[skmVSign];
           2:FDisplay.Signs:=FDisplay.Signs+[skVSign];
           3:FDisplay.Signs:=FDisplay.Signs+[skmASign];
           4:FDisplay.Signs:=FDisplay.Signs+[skASign];
           5:FDisplay.Signs:=FDisplay.Signs+[skRSign];
           6:FDisplay.Signs:=FDisplay.Signs+[skContSign];
-          7:FDisplay.Signs:=FDisplay.Signs+[skDSign];
+          7:FDisplay.Signs:=FDisplay.Signs+[skDSign];}
+          1:DisplaySigns:=DisplaySigns+[skmVSign];
+          2:DisplaySigns:=DisplaySigns+[skVSign];
+          3:DisplaySigns:=DisplaySigns+[skmASign];
+          4:DisplaySigns:=DisplaySigns+[skASign];
+          5:DisplaySigns:=DisplaySigns+[skRSign];
+          6:DisplaySigns:=DisplaySigns+[skContSign];
+          7:DisplaySigns:=DisplaySigns+[skDSign];
           end;
      if (Units<5) then {only mA,A,mV,V}
         if ((FRxData[1] and %00001000)>0) then begin
-           FDisplay.Signs:=FDisplay.Signs+[skACSign];
-           FDisplay.Signs:=FDisplay.Signs-[skDCSign];
+           {FDisplay.Signs:=FDisplay.Signs+[skACSign];
+           FDisplay.Signs:=FDisplay.Signs-[skDCSign];}
+           DisplaySigns:=DisplaySigns+[skACSign];
+           DisplaySigns:=DisplaySigns-[skDCSign];
            end else begin
-                  FDisplay.Signs:=FDisplay.Signs-[skACSign];
-                  FDisplay.Signs:=FDisplay.Signs+[skDCSign];
+                  {FDisplay.Signs:=FDisplay.Signs-[skACSign];
+                  FDisplay.Signs:=FDisplay.Signs+[skDCSign];}
+                  DisplaySigns:=DisplaySigns-[skACSign];
+                  DisplaySigns:=DisplaySigns+[skDCSign];
                end
            else begin
-                  FDisplay.Signs:=FDisplay.Signs-[skACSign];
-                  FDisplay.Signs:=FDisplay.Signs-[skDCSign];
+                  {FDisplay.Signs:=FDisplay.Signs-[skACSign];
+                  FDisplay.Signs:=FDisplay.Signs-[skDCSign];}
+                  DisplaySigns:=DisplaySigns-[skACSign];
+                  DisplaySigns:=DisplaySigns-[skDCSign];
              end;
      Range:=(FRxData[1] and %01110000);
      if Units=5 then
         case Range of
           0:begin
-                 FDisplay.Signs:=FDisplay.Signs-[skkRSign];
-                 FDisplay.Signs:=FDisplay.Signs+[skRSign];
+                 {FDisplay.Signs:=FDisplay.Signs-[skkRSign];
+                 FDisplay.Signs:=FDisplay.Signs+[skRSign];}
+                 DisplaySigns:=DisplaySigns-[skkRSign];
+                 DisplaySigns:=DisplaySigns+[skRSign];
             end;
           16,32,48,64,80:begin
-                  FDisplay.Signs:=FDisplay.Signs+[skkRSign];
-                  FDisplay.Signs:=FDisplay.Signs-[skRSign];
+                  {FDisplay.Signs:=FDisplay.Signs+[skkRSign];
+                  FDisplay.Signs:=FDisplay.Signs-[skRSign];}
+                  DisplaySigns:=DisplaySigns+[skkRSign];
+                  DisplaySigns:=DisplaySigns-[skRSign];
             end;
         end;
      //Function information
-     if (FRxData[2] and %00000001)>0 then FDisplay.Signs:=FDisplay.Signs+[skTHldSign] else FDisplay.Signs:=FDisplay.Signs-[skTHldSign];
+     {if (FRxData[2] and %00000001)>0 then FDisplay.Signs:=FDisplay.Signs+[skTHldSign] else FDisplay.Signs:=FDisplay.Signs-[skTHldSign];}
+     if (FRxData[2] and %00000001)>0 then DisplaySigns:=DisplaySigns+[skTHldSign] else DisplaySigns:=DisplaySigns-[skTHldSign];
      if (FRxData[2] and %00000010)>0 then begin
-        FDisplay.Signs:=FDisplay.Signs+[skMinSign];
-        FDisplay.Signs:=FDisplay.Signs+[skMaxSign];
+        {FDisplay.Signs:=FDisplay.Signs+[skMinSign];
+        FDisplay.Signs:=FDisplay.Signs+[skMaxSign];}
+        DisplaySigns:=DisplaySigns+[skMinSign];
+        DisplaySigns:=DisplaySigns+[skMaxSign];
         end else begin
-            FDisplay.Signs:=FDisplay.Signs-[skMinSign];
-            FDisplay.Signs:=FDisplay.Signs-[skMaxSign];
+            {FDisplay.Signs:=FDisplay.Signs-[skMinSign];
+            FDisplay.Signs:=FDisplay.Signs-[skMaxSign];}
+            DisplaySigns:=DisplaySigns-[skMinSign];
+            DisplaySigns:=DisplaySigns-[skMaxSign];
                end;
-     if (FRxData[2] and %00010000)>0 then FDisplay.Signs:=FDisplay.Signs+[skHzSign] else FDisplay.Signs:=FDisplay.Signs-[skHzSign];
+     {if (FRxData[2] and %00010000)>0 then FDisplay.Signs:=FDisplay.Signs+[skHzSign] else FDisplay.Signs:=FDisplay.Signs-[skHzSign];
      if (FRxData[2] and %00100000)>0 then FDisplay.Signs:=FDisplay.Signs+[skNullSign] else FDisplay.Signs:=FDisplay.Signs-[skNullSign];
-     if (FRxData[2] and %01000000)>0 then FDisplay.Signs:=FDisplay.Signs+[skAutoSign] else FDisplay.Signs:=FDisplay.Signs-[skAutoSign];
+     if (FRxData[2] and %01000000)>0 then FDisplay.Signs:=FDisplay.Signs+[skAutoSign] else FDisplay.Signs:=FDisplay.Signs-[skAutoSign];}
+     if (FRxData[2] and %00010000)>0 then DisplaySigns:=DisplaySigns+[skHzSign] else DisplaySigns:=DisplaySigns-[skHzSign];
+     if (FRxData[2] and %00100000)>0 then DisplaySigns:=DisplaySigns+[skNullSign] else DisplaySigns:=DisplaySigns-[skNullSign];
+     if (FRxData[2] and %01000000)>0 then DisplaySigns:=DisplaySigns+[skAutoSign] else DisplaySigns:=DisplaySigns-[skAutoSign];
      //Sign
-     if (FRxData[3] and %00000010)>0 then FDisplay.Signs:=FDisplay.Signs+[skNegSign] else FDisplay.Signs:=FDisplay.Signs-[skNegSign];
+     {if (FRxData[3] and %00000010)>0 then FDisplay.Signs:=FDisplay.Signs+[skNegSign] else FDisplay.Signs:=FDisplay.Signs-[skNegSign];}
+     if (FRxData[3] and %00000010)>0 then DisplaySigns:=DisplaySigns+[skNegSign] else DisplaySigns:=DisplaySigns-[skNegSign];
      //Numbers and decimal dot
      j:=2;
      NumDisplay:='';
@@ -199,48 +251,62 @@ begin
        end;
        inc(j);
      end;
+     FDisplay.Signs:=DisplaySigns;
      FDisplay.Text:=NumDisplay;
 end;
 
 function TCustomTTi1604Comm.GetMeasurement:string;
 var NumDisplay:string;
-    i,j:integer;
+    i:integer;
+    c:byte;
 begin
-     if FRxData[0] <>13 then exit;
+     //if FRxData[0] <>13 then exit;
      //Sign
-     if (FRxData[3] and %00000010)>0 then NumDisplay:='-' else NumDisplay:='';
-     //Numbers and decimal dot
-     j:=2;
-     //NumDisplay:='';
-     for i:=4 to 8 do begin
-       case FRxData[i] of
-            0:;//NumDisplay:=NumDisplay+' ';
-            252:NumDisplay:=NumDisplay+'0';
-            253:begin NumDisplay:=NumDisplay+'0'; inc(j); NumDisplay:=NumDisplay+'.'; end;
-            96:NumDisplay:=NumDisplay+'1';
-            97:begin NumDisplay:=NumDisplay+'1'; inc(j); NumDisplay:=NumDisplay+'.'; end;
-            218:NumDisplay:=NumDisplay+'2';
-            219:begin NumDisplay:=NumDisplay+'2'; inc(j); NumDisplay:=NumDisplay+'.'; end;
-            242:NumDisplay:=NumDisplay+'3';
-            243:begin NumDisplay:=NumDisplay+'3'; inc(j); NumDisplay:=NumDisplay+'.'; end;
-            102:NumDisplay:=NumDisplay+'4';
-            103:begin NumDisplay:=NumDisplay+'4'; inc(j); NumDisplay:=NumDisplay+'.'; end;
-            182:NumDisplay:=NumDisplay+'5';
-            183:begin NumDisplay:=NumDisplay+'5'; inc(j); NumDisplay:=NumDisplay+'.'; end;
-            190:NumDisplay:=NumDisplay+'6';
-            191:begin NumDisplay:=NumDisplay+'6'; inc(j); NumDisplay:=NumDisplay+'.'; end;
-            224:NumDisplay:=NumDisplay+'7';
-            225:begin NumDisplay:=NumDisplay+'7'; inc(j); NumDisplay:=NumDisplay+'.'; end;
-            254:NumDisplay:=NumDisplay+'8';
-            255:begin NumDisplay:=NumDisplay+'8'; inc(j); NumDisplay:=NumDisplay+'.'; end;
-            230:NumDisplay:=NumDisplay+'9';
-            231:begin NumDisplay:=NumDisplay+'9'; inc(j); NumDisplay:=NumDisplay+'.'; end;
-            142:;//NumDisplay:=NumDisplay+'F';
-            28:;//NumDisplay:=NumDisplay+'L';
+     NumDisplay:='';
+     Result:='';
+     FCommTimer.Enabled:=false;
+     FSerial.Purge;
+     if FSerial.CanRead(500) then begin
+       repeat
+         c:=FSerial.RecvByte(0);
+       until c=13;
+       FRxData[0]:=13;
+       for i:=1 to 9 do begin
+           FRxData[i]:=FSerial.RecvByte(10);
        end;
-       inc(j);
-     end;
-     result:=NumDisplay;
+       if (FRxData[3] and %00000010)>0 then NumDisplay:='-' else NumDisplay:='';
+       //Numbers and decimal dot
+       //NumDisplay:='';
+       for i:=4 to 8 do begin
+         case FRxData[i] of
+              0:;//NumDisplay:=NumDisplay+' ';
+              252:NumDisplay:=NumDisplay+'0';
+              253:begin NumDisplay:=NumDisplay+'0.'; end;
+              96:NumDisplay:=NumDisplay+'1';
+              97:begin NumDisplay:=NumDisplay+'1.'; end;
+              218:NumDisplay:=NumDisplay+'2';
+              219:begin NumDisplay:=NumDisplay+'2.';end;
+              242:NumDisplay:=NumDisplay+'3';
+              243:begin NumDisplay:=NumDisplay+'3.';end;
+              102:NumDisplay:=NumDisplay+'4';
+              103:begin NumDisplay:=NumDisplay+'4.';end;
+              182:NumDisplay:=NumDisplay+'5';
+              183:begin NumDisplay:=NumDisplay+'5.';end;
+              190:NumDisplay:=NumDisplay+'6';
+              191:begin NumDisplay:=NumDisplay+'6.';end;
+              224:NumDisplay:=NumDisplay+'7';
+              225:begin NumDisplay:=NumDisplay+'7.';end;
+              254:NumDisplay:=NumDisplay+'8';
+              255:begin NumDisplay:=NumDisplay+'8.';end;
+              230:NumDisplay:=NumDisplay+'9';
+              231:begin NumDisplay:=NumDisplay+'9.';end;
+              142:;//NumDisplay:=NumDisplay+'F';
+              28:;//NumDisplay:=NumDisplay+'L';
+         end;
+       end;
+       result:=NumDisplay;
+   end;
+   FCommTimer.Enabled:=not FDisableTimer;
 end;
 
 procedure TCustomTTi1604Comm.ReadComm(ASender:TObject);
@@ -248,6 +314,7 @@ var i:integer;
     c:byte;
 begin
      if FSerial.CanRead(0) then begin
+        FCommTimer.Enabled:=false;
        repeat
          c:=FSerial.RecvByte(0);
        until c=13;
@@ -256,6 +323,9 @@ begin
            FRxData[i]:=FSerial.RecvByte(10);
        end;
        if Assigned(FDisplay) then SendDataToDisplay;
+       GetMeasurement;
+       FSerial.Purge;
+       FCommTimer.Enabled:=not FDisableTimer;
      end;
 end;
 
@@ -284,6 +354,21 @@ end;
 procedure TCustomTTi1604Comm.SetPort(NewPort:string);
 begin
      FPort:=NewPort;
+end;
+
+procedure TCustomTTi1604Comm.SetDisableTimer(NewTimerStatus:boolean);
+begin
+     if FActive then FCommTimer.Enabled:=not NewTimerStatus;
+     FDisableTimer:=NewTimerStatus;
+end;
+
+procedure TCustomTTi1604Comm.SetUpdateInterval (value : cardinal);
+begin
+  if (value <> FUpdateInterval) then
+  begin
+    FUpdateInterval := value;
+    FCommTimer.Interval:=FUpdateInterval;
+  end;
 end;
 
 procedure TCustomTTi1604Comm.SetActive(NewActive:boolean);
@@ -332,7 +417,7 @@ begin
                    Sleep(500);
                    FSerial.Purge;
                    FActive:=true;
-                   FCommTimer.Enabled:=true;
+                   FCommTimer.Enabled:=not FDisableTimer;//true;
               end;
          end;
      end else begin
@@ -345,6 +430,7 @@ begin
            FSerial.SendString(cmdLocal);  //Local mode constant
            Sleep(1000);
            FSerial.CloseSocket;
+           FActive:=false;
            if Assigned(FDisplay) then begin
              FDisplay.Text:=TTi1604msgOFF;
              FDisplay.Signs:=[];
